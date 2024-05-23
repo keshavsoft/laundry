4e9f5817f64ca84d3ca5f31e64b22b8260c9cf54
Ch-Sai-Jaswanth <saijaswanthch2003@gmail.com>
2024-05-23 22:58:36 +0530

    Added ItemsWithSearchOnModalTable

--- public/Laundry/Branch/Orders/NewOrder/AddItems/Js/FormLoad/BuildBSTables/ItemsTable/OnLoadSuccess/TableFooterItemNameSearchButtonId/ItemsOnModalTable/EntryFile.js
+++ public/Laundry/Branch/Orders/NewOrder/AddItems/Js/FormLoad/BuildBSTables/ItemsTable/OnLoadSuccess/TableFooterItemNameSearchButtonId/ItemsOnModalTable/EntryFile.js
@@ -2,19 +2,26 @@ import { StartFunc as StartFuncOnClickRow } from "./onClickRow.js";
 
 const StartFunc = () => {
     let jVarLocalModalId = "ItemsOnModal";
-
-    var $table = $(document.getElementById(jVarLocalModalId).querySelector("table"));
-    $table.bootstrapTable({
-        data: jFLocalFromLocalStorage(),
-        onClickRow: StartFuncOnClickRow
-    });
+        var $table = $(document.getElementById(jVarLocalModalId).querySelector("table"));
+        $table.bootstrapTable({
+            data: jFLocalFromLocalStorage(),
+            onClickRow: StartFuncOnClickRow
+        });
+        $(`#${jVarLocalModalId}`).modal("show");
 };
 
 const jFLocalFromLocalStorage = () => {
     let jVarLocalDataFromLocalStorage = localStorage.getItem("ItemNames");
     let jVarLocalDataFromLocalStorageJson = JSON.parse(jVarLocalDataFromLocalStorage);
-
+    //console.log(jVarLocalDataFromLocalStorageJson);
     return jVarLocalDataFromLocalStorageJson;
 };
+
+/* const jFLocalFromLocalStorageWithFilter = (jFLocalFilter) => {
+    let jVarLocalDataFromLocalStorage = localStorage.getItem("ItemNames");
+    let jVarLocalDataFromLocalStorageJson = JSON.parse(jVarLocalDataFromLocalStorage);
+    
+    return jVarLocalDataFromLocalStorageJson.filter(obj => obj.ItemName.toLowerCase().includes(jFLocalFilter.toLowerCase()));
+}; */
 
 export { StartFunc };
