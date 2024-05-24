6ecca5c9d54af2dec42bcdd606dd4d7a4124919f
JAMPANA MAHEETH SAI DURGA VARMA <21131a0581@gvpce.ac.in>
2024-05-24 16:21:42 +0530

    Items Modals Aggregated into single one

--- public/Laundry/Branch/Orders/NewOrder/AddItems/Js/FormLoad/BuildBSTables/ItemsTable/OnLoadSuccess/TableFooterItemNameSearchButtonId/ButtonClickFunc.js
+++ public/Laundry/Branch/Orders/NewOrder/AddItems/Js/FormLoad/BuildBSTables/ItemsTable/OnLoadSuccess/TableFooterItemNameSearchButtonId/ButtonClickFunc.js
@@ -1,16 +1,17 @@
 import { StartFunc as StartFuncItemsOnModalTable } from "./ItemsOnModalTable/EntryFile.js";
-import { StartFunc as StartFuncItemsWithSearchOnModalTable } from "./ItemsWithSearchOnModal/EntryFile.js";
 
 const StartFunc = () => {
     // let jVarLocalModalId = "ItemsOnModal";
     let jFLocalFilter = document.getElementById("TableFooterItemNameId").value;
-    if(jFLocalFilter) {
-        StartFuncItemsWithSearchOnModalTable(jFLocalFilter);
-        //console.log(jFLocalFilter);
-    }
-    else {
-        StartFuncItemsOnModalTable();
-    }
+
+    StartFuncItemsOnModalTable(jFLocalFilter);
+    // if(jFLocalFilter) {
+    //     StartFuncItemsWithSearchOnModalTable(jFLocalFilter);
+    //     //console.log(jFLocalFilter);
+    // }
+    // else {
+    //     StartFuncItemsOnModalTable();
+    // }
 };
 
 export { StartFunc };
