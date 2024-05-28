5532e68d147be05e8a628aa911dc54a5c39fca02
Bhaskar-459 <m03bhaskar@gmail.com>
2024-05-28 16:43:13 +0530

    deleted an endpoint

--- Custom/Clients/Washtex/Qrcodes/dals/getFuncs/EntryFile.js
+++ Custom/Clients/Washtex/Qrcodes/dals/getFuncs/EntryFile.js
@@ -1,15 +1,6 @@
-import { StartFunc as StartFunReadFileById } from '../../kLowDb/CommonFuncs/GenerateQrCodes.js';
+// import { StartFunc as StartFunReadFileById } from '../../kLowDb/CommonFuncs/GenerateQrCodes.js';
 import { StartFunc as StartFunGenerateQrWithTable } from '../../kLowDb/CommonFuncs/GenerateQrWithTable/GenerateQrWithTable.js';
 
-let GetIdFunc = ({ inId }) => {
-    let LocalFromLowDb = StartFunReadFileById({ inId });
-
-    if (LocalFromLowDb === false) {
-        return false;
-    };
-
-    return LocalFromLowDb;
-};
 
 let GetIdWithTableFunc = ({ inTable, inId }) => {
     let LocalFromLowDb = StartFunGenerateQrWithTable({ inTable, inId });
@@ -22,4 +13,4 @@ let GetIdWithTableFunc = ({ inTable, inId }) => {
 };
 
 
-export { GetIdFunc, GetIdWithTableFunc };
+export { GetIdWithTableFunc };
