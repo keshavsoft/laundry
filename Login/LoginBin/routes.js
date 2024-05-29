f6a2e8fd400bf51c92758bfeb28c25067bde44d1
JAMPANA MAHEETH SAI DURGA VARMA <21131a0581@gvpce.ac.in>
2024-05-29 11:37:29 +0530

    Get and Post End points updated for the Login Functionality

--- Login/LoginBin/routes.js
+++ Login/LoginBin/routes.js
@@ -3,7 +3,9 @@ import express from 'express';
 var router = express.Router();
 
 import { router as routerFromCreateToken } from './CreateToken/routes.js';
+import { router as routerFromUsers } from "./Users/routes.js";
 
 router.use('/CreateToken', routerFromCreateToken);
+router.use('/Users', routerFromUsers);
 
 export { router };
