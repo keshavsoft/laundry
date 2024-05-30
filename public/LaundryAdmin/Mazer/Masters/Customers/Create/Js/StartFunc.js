6d5df64f1c17a617ef729a894b6d6fdc06d4e3ac
Sravanam charan <sravanamcharan20@gmail.com>
2024-05-30 14:25:07 +0530

    suggestions added in admin

--- public/LaundryAdmin/Mazer/Masters/Customers/Create/Js/StartFunc.js
+++ public/LaundryAdmin/Mazer/Masters/Customers/Create/Js/StartFunc.js
@@ -1,7 +1,11 @@
 import { StartFunc as StartFuncButtonClickFunc } from "./Addlisteners/SaveButtonClickId/ButtonClickFunc.js";
+import { StartFunc as ShowMobileList  } from "../../Js/ShowMobileList.js";
+import { StartFunc as ShowCustomerList  } from "../../Js/ShowCustomerList.js";
 
 const StartFunc = () => {
     StartFuncButtonClickFunc();
+    ShowMobileList();
+    ShowCustomerList();
 };
 
 StartFunc();
--- public/LaundryAdmin/Mazer/Masters/Items/Create/Js/StartFunc.js
+++ public/LaundryAdmin/Mazer/Masters/Items/Create/Js/StartFunc.js
@@ -1,7 +1,9 @@
 import { StartFunc as StartFuncButtonClickFunc } from "./Addlisteners/SaveButtonClickId/ButtonClickFunc.js";
+import { StartFunc as  ShowItemList} from "../../Js/ShowItemList.js";
 
 const StartFunc = () => {
     StartFuncButtonClickFunc();
+    ShowItemList();
 };
 
 StartFunc();
