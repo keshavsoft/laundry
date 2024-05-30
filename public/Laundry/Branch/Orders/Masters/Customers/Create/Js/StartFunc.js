bf98c71c6ce4762a909f6146cb27a77cd80534f8
Sravanam charan <sravanamcharan20@gmail.com>
2024-05-30 12:06:19 +0530

    suggestions for mobile number added

--- public/Laundry/Branch/Orders/Masters/Customers/Create/Js/StartFunc.js
+++ public/Laundry/Branch/Orders/Masters/Customers/Create/Js/StartFunc.js
@@ -1,7 +1,10 @@
 import { StartFunc as StartFuncButtonClickFunc } from "./Addlisteners/SaveButtonClickId/ButtonClickFunc.js";
+import { StartFunc as ShowMobileList  } from "../../Js/ShowMobileList.js";
+
 
 const StartFunc = () => {
     StartFuncButtonClickFunc();
+    ShowMobileList();
 };
 
 StartFunc();
