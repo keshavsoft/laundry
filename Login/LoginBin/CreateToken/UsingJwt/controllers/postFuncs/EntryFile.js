4aac82b6ad786896ef255c9800df042ccdbd5e73
dwijeshbing <dwijeshbingumalla@gmail.com>
2024-05-31 17:01:55 +0530

    changes made for login

--- Login/LoginBin/CreateToken/UsingJwt/controllers/postFuncs/EntryFile.js
+++ Login/LoginBin/CreateToken/UsingJwt/controllers/postFuncs/EntryFile.js
@@ -3,7 +3,7 @@ import { PostFunc as PostFuncRepo } from '../../repos/postFuncs/EntryFile.js';
 let PostFunc = async (req, res) => {
     let LocalData = req.body;
 
-    let LocalUsername = LocalData.Username;
+    let LocalUsername = LocalData.UserName;
     let LocalPassword = LocalData.Password;
 
     let LocalFromRepo = await PostFuncRepo({ inUsername: LocalUsername, inPassword: LocalPassword });
