c7772e46e039990779e46e0534f206aee9047a87
Indra Veerababu Kondi <indrakondi@gmail.com>
2024-06-04 18:18:20 +0530

    buildbstable is added in mazer

--- public/LaundryAdmin/Mazer/Masters/Customers/ShowAll/Js/FormLoad/StartFunc.js
+++ public/LaundryAdmin/Mazer/Masters/Customers/ShowAll/Js/FormLoad/StartFunc.js
@@ -1,6 +1,8 @@
+import { StartFunc as StartFuncBuildBSTables } from "./BuildBSTables/EntryFile.js";
 import { StartFunc as StartFuncAddlisteners } from "./ListenerFuncs/StartFunc.js";
 
 const StartFunc = () => {
+    StartFuncBuildBSTables();
     StartFuncAddlisteners();
 };
 
