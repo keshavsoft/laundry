a20efdd191afb23efdfc0982f9fb24bc6ea3aae7
Srivasu215 <sri.vasu215@gmail.com>
2024-06-04 17:59:28 +0530

    SREENIVAS

--- public/Laundry/Branch/Today/QrCodePrint/MultiPrintQr/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromQrCodes/FetchHeaders/EntryFile.js
+++ public/Laundry/Branch/Today/QrCodePrint/MultiPrintQr/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromQrCodes/FetchHeaders/EntryFile.js
@@ -3,7 +3,7 @@ import KeysJson from './Keys.json' assert {type: 'json'};
 let StartFunc = () => {
     let jVarLocalBodyKeys = {};
     jVarLocalBodyKeys.FindValue = `${new Date().toLocaleDateString('en-GB')}`;
-    jVarLocalBodyKeys.FindKey = "new Date(e.OrderData.Currentdateandtime).toLocaleDateString('en-GB')";
+    jVarLocalBodyKeys.FindKey = "new Date(e.BookingData.OrderData.Currentdateandtime).toLocaleDateString('en-GB')";
 
     KeysJson.body = JSON.stringify(jVarLocalBodyKeys);
 
