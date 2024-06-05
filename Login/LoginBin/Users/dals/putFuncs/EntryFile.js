b27d3993c15a598d111433a74169462c7caf9ed8
dwijeshbing <dwijeshbingumalla@gmail.com>
2024-06-05 18:57:25 +0530

    made ismail value as true

--- Login/LoginBin/Users/dals/putFuncs/EntryFile.js
+++ Login/LoginBin/Users/dals/putFuncs/EntryFile.js
@@ -1,7 +1,9 @@
 import { StartFunc as StartFuncUpdateData } from '../../kLowDb/UpdateData.js';
+import { StartFunc as StartFuncUpdateDataEmail } from '../../kLowDb/UpdateData/Email.js';
 
 let PutFunc = ({ inDataToUpdate, inId }) => {
-    return StartFuncUpdateData({ inDataToUpdate, inId });
+    // return StartFuncUpdateData({ inDataToUpdate, inId });
+    return StartFuncUpdateDataEmail({ inUuid : inId});
 };
 
 export { PutFunc };
