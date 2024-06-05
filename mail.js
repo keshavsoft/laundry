cb4a557532fcf193ceba9c3c70fdb7e4348fbcf8
dwijeshbing <dwijeshbingumalla@gmail.com>
2024-06-05 17:22:11 +0530

    email messages sent

---
+++ mail.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: "21131a0522@gvpce.ac.in",
+        pass: "",
+    },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+        from: '"Bingumalla Dwijesh" <21131a0522@gvpce.ac.in>', // sender address
+        to: "dwijeshbingumalla@gmail.com, nknnkumar@yahoo.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
