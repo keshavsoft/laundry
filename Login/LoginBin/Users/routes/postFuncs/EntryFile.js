0b6986a283a4514833a36b05fc9c40ef40443852
keshavsoft <nknnkumar@yahoo.com>
2024-06-06 11:19:47 +0530

    keshav

--- Login/LoginBin/Users/routes/postFuncs/EntryFile.js
+++ Login/LoginBin/Users/routes/postFuncs/EntryFile.js
@@ -1,13 +1,13 @@
 import express from 'express';
 var router = express.Router();
 
-import { PostFunc , PostFuncWithMail as PostFuncWithMail, PostFuncEndUser as PostFuncForUser} from '../../controllers/postFuncs/EntryFile.js';
+import { PostFunc, PostFuncWithMail as PostFuncWithMail, PostFuncEndUser as PostFuncForUser } from '../../controllers/postFuncs/EntryFile.js';
 import { PostFunc as PostFuncMiddleWare } from "../../Middlewares/postFuncs/EntryFile.js";
 import { PostFunc as PostFuncWithMailMiddleWare } from "../../Middlewares/postFuncs/1-EntryFile.js";
-
 
 router.post('/', PostFuncMiddleWare, PostFunc);
 router.post("/withMail", PostFuncWithMailMiddleWare, PostFuncWithMail);
 router.post("/endUser", PostFuncWithMailMiddleWare, PostFuncForUser);
+//withDataPk, DataPk
 
 export { router };
