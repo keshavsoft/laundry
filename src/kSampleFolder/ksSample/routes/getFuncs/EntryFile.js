021da8238839a79da265f3c240cd24c011f164e6
JAMPANA MAHEETH SAI DURGA VARMA <21131a0581@gvpce.ac.in>
2024-06-07 16:45:31 +0530

    SRC files changed

--- src/kSampleFolder/ksSample/routes/getFuncs/EntryFile.js
+++ src/kSampleFolder/ksSample/routes/getFuncs/EntryFile.js
@@ -3,27 +3,10 @@ import express from 'express';
 var router = express.Router();
 
 import {
-    GetFunc, GetDataOnlyFunc, GetFromModalUuidFunc, GetFromModalUuidAndTSFunc,
-    GetFromModalFunc, GetIdFunc, GetBodyCheckFunc, GetRowCountFunc,
-    GetRowDataFunc, GetRowCountByIdFunc, GetFilterDataFunc, GetSummeryFunc, GetMaxRowFunc,
-    GetNonMaxRowFunc
+    GetFunc, GetDataOnlyFunc
 } from '../../controllers/getFuncs/EntryFile.js';
 
-router.get('/FromModal', GetFromModalFunc);
 router.get('/', GetFunc);
-router.get('/Summery', GetSummeryFunc);
-router.get('/FromModalUuid', GetFromModalUuidFunc);
-router.get('/FromModalUuidAndTS', GetFromModalUuidAndTSFunc);
 router.get('/DataOnly', GetDataOnlyFunc);
-router.get('/BodyCheck', GetBodyCheckFunc);
-router.get('/RowCount', GetRowCountFunc);
-router.get('/RowCount/:inKey/:inValue', GetRowCountByIdFunc);
-
-router.get('/RowData/:inKey/:inValue', GetRowDataFunc);
-router.get('/FilterData/:inKey/:inValue', GetFilterDataFunc);
-router.get('/MaxRow', GetMaxRowFunc);
-
-router.get('/NonMaxRow/:id', GetNonMaxRowFunc);
-router.get('/:id', GetIdFunc);
 
 export { router };
