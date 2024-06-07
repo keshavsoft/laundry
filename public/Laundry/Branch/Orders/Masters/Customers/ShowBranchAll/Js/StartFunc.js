85bdb5c317de1d50a445eaa8941e28be562a9b1f
Indra Veerababu Kondi <indrakondi@gmail.com>
2024-06-07 18:45:45 +0530

    Login Model added for show branch all

--- public/Laundry/Branch/Orders/Masters/Customers/ShowBranchAll/Js/StartFunc.js
+++ public/Laundry/Branch/Orders/Masters/Customers/ShowBranchAll/Js/StartFunc.js
@@ -2,14 +2,15 @@
 import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
 import { StartFunc as StartFuncAfterDomLoad } from "./AfterDomLoad/StartFunc.js";
+import { StartFunc as StartFuncLoginCheck } from "/LaundryAdmin/Js/LoginModal/EntryFile.js";
+import { StartFunc as StartFuncFetchFromFuncs } from "./FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromFuncs/Entry.js";
 
-const StartFunc = () => {
+const StartFunc = async() => {
     // StartFuncNavigation();
+    StartFuncFormLoad();
 
-    let jVarLocalFromAdmin = true;
-
+    let jVarLocalFromAdmin = await StartFuncLoginCheck({ inSuccessFunc: StartFuncFetchFromFuncs });
     if (jVarLocalFromAdmin) {
-        StartFuncFormLoad();
 
         StartFuncShowOnDom();
 
