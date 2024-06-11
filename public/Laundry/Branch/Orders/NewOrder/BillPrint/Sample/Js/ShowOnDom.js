bd4335d20e9f880bc9e2e2e9e10a3fae4cefaa2e
Keshav Nalam <nknnumar@gmail.com>
2024-06-11 07:48:48 +0200

    keshav

--- public/Laundry/Branch/Orders/NewOrder/BillPrint/Sample/Js/ShowOnDom.js
+++
@@ -1,7 +0,0 @@
-import { StartFunc as StartFuncButtonClickFunc } from "./FetchFuncs/FromServer/OrdersData/2-ButtonClickFunc.js";
-
-let StartFunc = async () => {
-    await StartFuncButtonClickFunc();
-};
-
-export { StartFunc }
