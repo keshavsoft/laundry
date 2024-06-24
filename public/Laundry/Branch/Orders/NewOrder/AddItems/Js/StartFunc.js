87911483598bec5af50b298251925c87b72a2424
Srivasu215 <sri.vasu215@gmail.com>
2024-06-24 20:24:00 +0530

    SREENIVAS

--- public/Laundry/Branch/Orders/NewOrder/AddItems/Js/StartFunc.js
+++ public/Laundry/Branch/Orders/NewOrder/AddItems/Js/StartFunc.js
@@ -1,9 +1,10 @@
 // import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
 import { StartFunc as StartFuncShowToDom } from "./ShowToDom/EntryFile.js";
+import { StartFunc as StartFuncLoginCheck } from "/Laundry/Js/LoginModal/EntryFile.js";
 
 const StartFunc = async () => {
-    let jVarLocalFromAdmin = true;
+    let jVarLocalFromAdmin = await StartFuncLoginCheck({ inSuccessFunc: StartFuncShowToDom});
 
     if (jVarLocalFromAdmin) {
         await StartFuncFormLoad();
