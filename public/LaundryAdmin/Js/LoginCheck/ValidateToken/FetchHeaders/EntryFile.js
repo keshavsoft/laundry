e53498600ab08c74e976d77a148dd4005e0297c1
keshavsoft <nknnkumar@yahoo.com>
2024-07-04 11:44:12 +0530

    keshav

--- public/LaundryAdmin/Js/LoginCheck/ValidateToken/FetchHeaders/EntryFile.js
+++ public/LaundryAdmin/Js/LoginCheck/ValidateToken/FetchHeaders/EntryFile.js
@@ -1,11 +1,9 @@
-import KeysJson from './Keys.json' assert {type: 'json'};
+import KeysJson from './Keys.json' with {type: 'json'};
 import { StartFunc as StartFuncPrepareBody } from "./PrepareBody.js";
 
 let StartFunc = () => {
     KeysJson.body = JSON.stringify(StartFuncPrepareBody());
     return KeysJson;
 };
-
-
 
 export { StartFunc };
