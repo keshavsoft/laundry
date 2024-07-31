03b7c08f22d49372120c0fea0cc6b851b4ece3e1
Srivasu215 <sri.vasu215@gmail.com>
2024-07-31 12:20:10 +0530

    SREENIVAS

--- public/LaundryAdmin/Mazer/Masters/AddOns/Delete/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromFuncs/PostFetch.js
+++ public/LaundryAdmin/Mazer/Masters/AddOns/Delete/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromFuncs/PostFetch.js
@@ -1,5 +1,5 @@
 let StartFunc = async () => {
-    let jVarLocalFetchUrl = `/bin/Masters/CustomerNames/DataOnly`
+    let jVarLocalFetchUrl = `/AdminFixedPk/Masters/CustomerNames/DataOnly`
     let response = await fetch(jVarLocalFetchUrl);
 
     return await response;
