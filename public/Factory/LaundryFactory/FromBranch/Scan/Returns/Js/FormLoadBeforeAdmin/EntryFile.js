3a677e3fd731c086b582bad9c86ff2c46efd63d8
Srivasu215 <sri.vasu215@gmail.com>
2024-08-27 19:51:18 +0530

    Completion

---
+++ public/Factory/LaundryFactory/FromBranch/Scan/Returns/Js/FormLoadBeforeAdmin/EntryFile.js
@@ -0,0 +1,9 @@
+import { StartFunc as StartFuncNavMenu } from "./NavMenu/Entry.js";
+import { StartFunc as StartFuncBreadCumMenu } from "./BreadCumMenu/Entry.js";
+
+const StartFunc = () => {
+    StartFuncNavMenu();
+    // StartFuncBreadCumMenu();
+};
+
+export { StartFunc };
