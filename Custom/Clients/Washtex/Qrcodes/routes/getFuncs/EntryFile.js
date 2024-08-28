dfd601aa3dbdcd84ba1de8676ab5047f5faa579c
keshavsoft <nknnkumar@yahoo.com>
2024-08-28 10:59:03 +0530

    custom qrcode api started

--- Custom/Clients/Washtex/Qrcodes/routes/getFuncs/EntryFile.js
+++ Custom/Clients/Washtex/Qrcodes/routes/getFuncs/EntryFile.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 import { GetIdWithTableFunc, GetHomeFunc } from '../../controllers/getFuncs/EntryFile.js';
 
-router.get('/Home', GetHomeFunc);
+router.get('/', GetHomeFunc);
+router.get('/Branch/:inBranchName', GetHomeFunc);
+router.get('/Date/:inDate', GetHomeFunc);
+router.get('/Today', GetHomeFunc);
+router.get('/BranchDate/:inBranchName/:inDate', GetHomeFunc);
+router.get('/BranchToday', GetHomeFunc);
+
 router.get('/Table/:inTable/:id', GetIdWithTableFunc);
 
 export { router };
